84df3f18273143914ee8cbd7e4023c56d9644fc5
FeLonCar1107 <pipe20001107@gmail.com>
2022-11-11 18:54:37 -0500

    enviar mensaje de texto

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const cors = require("cors");
+
 const app = express();
 const { API_VERSION } = require("./config");
 
+const userRoutes = require("./src/routes/userRoutes")
+
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
+// Evitar el bloqueo al CORS
+app.use(cors());
+
+app.use(`/api/${API_VERSION}`, userRoutes);
 
 /* Configuración de los header HTTP */
 module.exports = app;
--- index.js
+++ index.js
@@ -32,3 +32,17 @@ app.get('/',(req,res)=>{
 
 
 app.use("/api/v1/", personRoute);
+
+require('dotenv').config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'Este es el mensaje desde twilio',
+     from: '+17088347783',
+     to: '+573014072955'
+   })
+  .then(message => console.log(message.sid));
